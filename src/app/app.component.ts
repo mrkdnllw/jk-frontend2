d92a526b9afcee9a0f3764cbac0d65f9427f6c73
mrkdnllw <mrkdnlph@gmail.com>
2023-04-18 01:31:33 +0800

    update to angu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ template: `
+ <div class="container-md">
+   <router-outlet></router-outlet>
+ </div>
+ `
 })
-export class AppComponent {
-  title = 'angular-app-heroku';
-}
+export class AppComponent { }
